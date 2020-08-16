a621a4dd89b69599dc637eb2c664314c2110abb7
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2020-08-16 16:59:34 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-// Import stylesheets
-import './style.css';
+// https://stackoverflow.com/questions/3512471/what-is-a-non-capturing-group-in-regular-expressions
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const value1 = "https://alpine.global.ssl.fastly.net/alpine/v3.12/community/x86_64/APKINDEX.tar.gz?test=123&foo=ABC&token=token_value"
+
+function replaceParam(value: string, newValue: string): string {
+  return value.replace(/(src=).*?(&)/g, '$1' + newValue + '$2');
+}
+
+
+
+
