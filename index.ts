566a00eacb2e18e13887c273060987b6f07560a1
ValeryShvyndzikau <valery_shvyndzikau@epam.com>
2020-08-16 17:18:20 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,12 +1,31 @@
 // https://stackoverflow.com/questions/3512471/what-is-a-non-capturing-group-in-regular-expressions
+// https://usefulangle.com/post/78/javascript-get-url-parameters
+
+const value1 = "https://usefulangle.com/post/78/javascript-get-url-parameters?test=123&foo=ABC&token=token_value&more=123"
+
+function replaceUrlParam(url, paramName, paramValue) {
+    if (paramValue == null) {
+        paramValue = '';
+    }
+    var pattern = new RegExp('\\b('+paramName+'=).*?(&|#|$)');
+    if (url.search(pattern)>=0) {
+        return url.replace(pattern,'$1' + paramValue + '$2');
+    }
+    url = url.replace(/[?#]$/,'');
+    return url + (url.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue;
+}
 
 
-const value1 = "https://alpine.global.ssl.fastly.net/alpine/v3.12/community/x86_64/APKINDEX.tar.gz?test=123&foo=ABC&token=token_value"
+function replaceParam(url: string, paramName: string, newValue: string): string {
+  const urlObject = new URL(url);
 
-function replaceParam(value: string, newValue: string): string {
-  return value.replace(/(src=).*?(&)/g, '$1' + newValue + '$2');
+  if (urlObject.searchParams.has(paramName)) {
+    urlObject.searchParams.set(paramName, newValue)
+  }
+
+  return urlObject.href;
 }
 
 
 
-
+console.log(replaceParam(value1, "token", '123'))
